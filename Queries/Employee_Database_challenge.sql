638d0a28cfaee4c6ed1e7a198bc98cf156b1a7f6
HelyxM <manningharry@gmail.com>
2021-11-28 19:36:55 -0500

    adding challenge files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,38 @@
+-- DELIVERABLE 1:
+
+-- Create new table for retiring employees
+SELECT e.emp_no, e.first_name, e.last_name,
+t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no, rt.first_name, rt.last_name, rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Determine count of titles retiring
+SELECT COUNT (ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+-- DELIVERABLE 2:
+
+-- Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN department_employees as de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as t 
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
